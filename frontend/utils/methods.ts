c3d2b13f37f9b9cbfdd02a3542c576b47e961df2
Poxie <poxen12@hotmail.com>
2022-01-03 23:55:59 +0100

    Added utility functions, fetch get option

---
+++ frontend/utils/methods.ts
@@ -0,0 +1,23 @@
+const API_ENDPOINT = process.env.NEXT_PUBLIC_API_ENDPOINT;
+
+// Accessing the root query property name from GraphQL
+const getRootQuery = (query: string) => {
+    const rootQuery = query.split('{')[1].split('(')[0].trim();
+    return rootQuery;
+}
+
+// GraphQL get request
+export const get = async (query: string) => {
+    return await fetch(`${API_ENDPOINT}`, {
+        method: 'POST',
+        body: JSON.stringify({ query }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+    }).then(res => res.json()).then(response => {
+        // Returning the data without graphql's 'data' and root query properties
+        // Essentially returning pure data
+        return response.data[getRootQuery(query)]
+    });
+}
