f0ba54ecd93bcb808673b5da9b2c9cfbbe247bdc
Poxie <poxen12@hotmail.com>
2022-01-04 00:31:09 +0100

    Added SEO for user page

---
+++ frontend/pages/[userId].tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import { getUserAvatar, getUserById } from '../utils';
+import Head from 'next/head';
+import { UserType } from '../utils/types';
+
+type Props = {
+    user: UserType;
+}
+export default function User(props: Props) {
+    const { user } = props;
+
+    return(
+        <div>
+            <Head>
+                <title>
+                    {user.name} | {process.env.NEXT_PUBLIC_WEBSITE_NAME}
+                </title>
+                <meta property="og:title" content={`${user.name}`} />
+
+                {/* Add this with bio later */}
+                <meta property="og:description" />
+
+                <meta property="og:image" content={getUserAvatar(user.avatar)} />
+            </Head>
+        </div>
+    )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const userId = context.params.userId;
+
+    // If is array, just use first index as userId
+    const user = await getUserById(Array.isArray(userId) ? userId[0] : userId);
+
+    return {
+        props: {
+            user
+        }
+    }
+}
