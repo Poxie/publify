c8dcb9c520f034559ea76679031070ec633c867d
Poxie <poxen12@hotmail.com>
2022-01-04 00:09:09 +0100

    Removed hardcoded 'database'

--- backend/resolvers/Query.ts
+++ backend/resolvers/Query.ts
@@ -1,8 +1,5 @@
 import { getPostById, getPostsByAuthorId, getUserById } from "../logic/db-actions";
 
-export const users = [{id: "1", name: 'Poxen', avatar: 'hello'}, {id: "2", name: 'Marre', avatar: 'Hello2'}];
-export const posts = [{id: "1", content: 'Posty1', authorId: "1"}, {id: "2", content: "post 24", authorId: "1"}, {id: "3", content: 'Post 3', authorId: "2"}]
-
 export const Query = {
     getUserById: async (parent: any, args: any) => {
         const userId = args.id;
