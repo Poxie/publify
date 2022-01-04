514361847e99f251fbf9782b2bf3e60abb50dfeb
Poxie <poxen12@hotmail.com>
2022-01-04 01:08:44 +0100

    Added flex component

---
+++ frontend/components/Flex.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { forwardRef } from "react";
+
+interface Props {
+    children: any;
+    className?: string;
+    flexDirection?: 'column' | 'row';
+    justifyContent?: 'center' | 'space-between' | 'space-around' | 'flex-end' | 'flex-start';
+    alignItems?: 'center' | 'flex-end' | 'flex-start';
+    flexWrap?: 'no-wrap' | 'wrap';
+    style?: any;
+    onClick?: () => void;
+    ref?: any;
+}
+
+export const Flex: React.FC<Props> = forwardRef<HTMLDivElement, Props>(({ children, className, flexDirection, flexWrap, justifyContent, alignItems, onClick, style: styling }, ref) => {
+    const style: any = {
+        display: 'flex',
+        flexDirection,
+        flexWrap,
+        justifyContent,
+        alignItems,
+        ...styling
+    }
+    className = className ? `${className} flex` : 'flex';
+
+    return(
+        <div style={style} className={className} onClick={onClick} ref={ref}>
+            {children}
+        </div>
+    )
+})
