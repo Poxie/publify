366d3faa1cf2e2ea34b9b9d61e9caed0b96008c7
Poxie <poxen12@hotmail.com>
2022-01-07 11:46:56 +0100

    Allowing redux devtools extension

--- frontend/redux/store.ts
+++ frontend/redux/store.ts
@@ -3,7 +3,9 @@ import reducers from './reducers';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
-export const store = createStore(reducers, applyMiddleware(thunk, logger));
+// @ts-ignore
+const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
+export const store = createStore(reducers, {}, composeEnhancers(applyMiddleware(thunk, logger)));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
