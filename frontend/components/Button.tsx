10f7b844168b544f36fd624b3bf29382aafdc48a
Poxie <poxen12@hotmail.com>
2022-01-07 16:08:35 +0100

    Added transparent type to button

--- frontend/components/Button.tsx
+++ frontend/components/Button.tsx
@@ -2,14 +2,26 @@ import styles from '../styles/Button.module.scss';
 
 interface Props {
     children: any;
-    type?: 'primary' | 'secondary';
+    type?: 'primary' | 'secondary' | 'transparent';
     style?: any;
     className?: any;
     onClick?: () => void;
 }
 
 export const Button: React.FC<Props> = ({ children, type='primary', style, className, onClick }) => {
-    const newClassName = [className, styles['button'], type === 'primary' ? styles['primary'] : styles['secondary']].join(' ');
+    let typeStyle: any;
+    switch(type) {
+        case 'primary':
+            typeStyle = styles['primary'];
+            break;
+        case 'secondary':
+            typeStyle = styles['secondary'];
+            break;
+        case 'transparent':
+            typeStyle = styles['transparent']
+            break;
+    }
+    const newClassName = [className, styles['button'], typeStyle].join(' ');
     return(
         <div className={newClassName} style={style} onClick={onClick}>
             {children}
