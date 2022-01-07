9e0e92415ed7311ce241efbb6e8d605716d45b52
Poxie <poxen12@hotmail.com>
2022-01-07 21:28:22 +0100

    Notification destroy and reset logic in notification layer

--- frontend/layouts/NotificationLayout.tsx
+++ frontend/layouts/NotificationLayout.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { destroyNotification, resetNotification } from '../redux/actions';
 import { useAppSelector } from '../redux/hooks';
 import { selectNotification, selectNotificationState, selectNotificationStatus } from '../redux/selectors';
 import styles from '../styles/Notifications.module.scss';
@@ -7,16 +9,29 @@ type Props = {
     children: any;
 }
 export const NotificationLayout: React.FC<Props> = ({ children }) => {
+    const dispatch = useDispatch();
     const notification = useAppSelector(state => selectNotification(state));
     const notificationStatus = useAppSelector(state => selectNotificationStatus(state));
     const notificationState = useAppSelector(state => selectNotificationState(state));
+
+    useEffect(() => {
+        if(!notificationState) return;
+
+        setTimeout(() => {
+            dispatch(destroyNotification());
+    
+            setTimeout(() => {
+                dispatch(resetNotification());
+            }, 400);
+        }, 5000);
+    }, [notificationState]);
 
     const notificationStyles = [styles['notification'], styles[notificationStatus], notificationState ? styles['animate-in'] : styles['animate-out']].join(' ');
     return(
         <>
             {children}
             {notification && (
-                <div className={notificationStyles}>
+                <div className={notificationStyles} key={Math.random()}>
                     {notification}
                 </div>
             )}
