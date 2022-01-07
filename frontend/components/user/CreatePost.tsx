0f16c197fd7aa34ec49e3ac6e8b11c8e51ecb20c
Poxie <poxen12@hotmail.com>
2022-01-07 19:38:19 +0100

    Created create post modal

---
+++ frontend/components/user/CreatePost.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useAuth } from '../../contexts/AuthProvider';
+import { useModal } from '../../contexts/ModalProvider';
+import { CreatePostModal } from '../../modals/create-post/CreatePostModal';
+import styles from '../../styles/User.module.scss';
+import { Avatar } from '../Avatar';
+import { Flex } from '../Flex';
+
+export const CreatePost = () => {
+    const { user } = useAuth();
+    const { setModal } = useModal();
+
+    return(
+        <Flex 
+            className={styles['create-post']}
+            alignItems={'center'}
+            onClick={() => setModal(<CreatePostModal />)}
+        >
+            <Avatar 
+                avatar={user?.avatar}
+                name={user?.displayName}
+                className={styles['create-post-avatar']}
+                size={28}
+            />
+            <span>
+                What is on your mind?
+            </span>
+        </Flex>
+    )
+}
