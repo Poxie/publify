01feec53663cd582acfd87e00f2b513da1bf5a77
Poxie <poxen12@hotmail.com>
2022-01-07 22:41:30 +0100

    Added types to selectors

--- frontend/redux/selectors.ts
+++ frontend/redux/selectors.ts
@@ -1,18 +1,18 @@
 import { createSelector } from "reselect";
-import { PostType } from "../utils/types";
+import { Media, PostType } from "../utils/types";
 import { RootState } from "./store";
 
-export const selectPosts = (state: RootState) => state.posts.posts;
-export const selectPostId = (state: RootState, postId: string) => postId;
-export const selectPostById = createSelector(
+export const selectPosts: (state: RootState) => PostType[] = (state) => state.posts.posts;
+export const selectPostId: (state: RootState, postId: string) => string = (state, postId) => postId;
+export const selectPostById: (state: RootState, postId: string) => PostType = createSelector(
     [selectPosts, selectPostId],
     (posts, postId) => posts.find(post => post.id === postId) 
 )
-export const selectPostIds = createSelector(
+export const selectPostIds: (state: RootState) => string[] = createSelector(
     [selectPosts],
     (posts) => posts.map(post => post.id)
 )
-export const selectPostMedia = createSelector(
+export const selectPostMedia: (state: RootState) => Media[] = createSelector(
     [selectPosts],
     (posts) => {
         let media = [];
