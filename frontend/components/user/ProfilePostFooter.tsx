c351e1217510eb89f21f35a96b9deab87ec85f56
Poxie <poxen12@hotmail.com>
2022-01-10 17:45:56 +0100

    Added expand post button

--- frontend/components/user/ProfilePostFooter.tsx
+++ frontend/components/user/ProfilePostFooter.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import styles from '../../styles/User.module.scss';
-import { HeartIcon } from '../../icons/HeartIcon';
 import { Flex } from '../Flex';
-import { CommentsIcon } from '../../icons/CommentsIcon';
 import { LikeButton } from '../LikeButton';
 import { CommentButton } from './CommentButton';
+import { ArrowIcon } from '../../icons/ARrowIcon';
+import { useRouter } from 'next/router';
 
 type Props = {
     postId: string;
 }
 export const ProfilePostFooter: React.FC<Props> = ({ postId }) => {
+    const router = useRouter();
+
     return(
-        <Flex className={styles['post-footer']}>
-            <LikeButton
-                postId={postId}
-            />
-            <CommentButton 
-                postId={postId}
-            />
+        <Flex className={styles['post-footer']} justifyContent={'space-between'}>
+            <Flex>
+                <LikeButton
+                    postId={postId}
+                />
+                <CommentButton 
+                    postId={postId}
+                />
+            </Flex>
+            <Flex 
+                alignItems={'center'}
+                className={styles['expand-post']}
+                onClick={() => router.push(`/posts/${postId}`)}
+            >
+                <span>
+                    Expand
+                </span> 
+                <ArrowIcon />
+            </Flex>
         </Flex>
     )
 }
