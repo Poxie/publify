5580b50e8540d5b07821c2faa68aa02a06ee23da
Poxie <poxen12@hotmail.com>
2022-01-11 20:20:35 +0100

    Added options component

---
+++ frontend/components/Options.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useRef, useState } from 'react';
+import styles from '../styles/Options.module.scss';
+
+type Item = {
+    text: string;
+    onClick: () => void;
+    type: 'default' | 'danger';
+}
+type Props = {
+    items: Item[];
+    children: any;
+}
+
+const OptionItem: React.FC<Item> = ({ text, onClick, type }) => {
+    const className = [styles['item'], type === 'danger' ? styles['danger'] : ''].join(' ');
+    return(
+        <div 
+            className={className}
+            onClick={onClick}
+        >
+            {text}
+        </div>
+    )
+}
+export const Options: React.FC<Props> = ({ items, children }) => {
+    const [visible, setVisible] = useState(false);
+    const ref = useRef<HTMLDivElement>(null);
+    
+    // Detecting click outside
+    useEffect(() => {
+        if(!visible) return;
+        const handleClickOutside = (e: MouseEvent) => {
+            // @ts-ignore
+            if(ref.current && !ref.current.contains(e.target)) {
+                toggleVisible();
+            }
+        }
+
+        // Bind the event listener
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            // Unbind the event listener on clean up
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [visible]);
+
+    // Toggling options menu
+    const toggleVisible = () => {
+        setVisible(previous => !previous);
+    }
+
+    return(
+        <div ref={ref}>
+            <div onClick={toggleVisible}>
+                {children}
+            </div>
+            {visible && (
+                <div className={styles['item-container']}>
+                    {items.map(item => {
+                        return(
+                            <OptionItem 
+                                {...item}
+                                key={item.text}
+                            />
+                        )
+                    })}
+                </div>
+            )}
+        </div>
+    )
+}
