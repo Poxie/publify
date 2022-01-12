2679ab3f2f0123a7f351de51ff90bcdf26696e9a
Poxie <poxen12@hotmail.com>
2022-01-12 23:25:50 +0100

    Preserving scroll on navigation

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,5 +2,8 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: [process.env.NEXT_PUBLIC_IMAGE_HOST, 'cdn.discordapp.com']
+  },
+  experimental: {
+    scrollRestoration: true
   }
 }
