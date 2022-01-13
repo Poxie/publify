33a6e16ce0a2b7d78431c32a5b36913900a090c9
Poxie <poxen12@hotmail.com>
2022-01-13 14:21:45 +0100

    Loading more posts on scroll

--- frontend/components/user/ProfileOverview.tsx
+++ frontend/components/user/ProfileOverview.tsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import styles from '../../styles/User.module.scss';
 import { useRouter } from 'next/router';
 import { Flex } from '../Flex';
 import { ProfilePosts } from './ProfilePosts';
 import { ProfileMediaPreview } from './ProfileMediaPreview';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUserPosts } from '../../redux/actions';
+import { fetchUserPosts, loadMorePosts } from '../../redux/actions';
 import { useAppSelector } from '../../redux/hooks';
 import { selectPostIds } from '../../redux/selectors';
 import { CreatePost } from './CreatePost';
@@ -16,17 +16,45 @@ export const ProfileOverview = () => {
     const dispatch = useDispatch();
     const postIds = useAppSelector(state => selectPostIds(state));
     const loading = useAppSelector(state => state.posts.loading);
+    const loadingMore = useRef(false);
 
-    useEffect(() => {
-        let isMounted = true;
+    const getUserId = useMemo(() => () => {
         let { userId } = router.query;
 
         // Makes sure userId is
         userId = Array.isArray(userId) ? userId[0] : userId;
 
-        // Fetching user posts
-        dispatch(fetchUserPosts(userId));
+        return userId
     }, [router.query]);
+
+    useEffect(() => {
+        let isMounted = true;
+        const userId = getUserId();
+
+        // Fetching user posts
+        dispatch(fetchUserPosts(userId, 0, postIds.length || 3));
+    }, [getUserId]);
+
+    useEffect(() => {
+        const listenForScroll = (e: Event) => {
+            const scrollPercentage = ((window.innerHeight + window.scrollY) / (document.body.offsetHeight)) * 100;
+            if(scrollPercentage > 90 && !loadingMore.current) {
+                const userId = getUserId();
+
+                loadingMore.current = true;
+                dispatch(loadMorePosts(userId, postIds.length, postIds.length + 4));
+                document.removeEventListener('scroll', listenForScroll);
+            }
+        }
+
+        document.addEventListener('scroll', listenForScroll);
+
+        return () => document.removeEventListener('scroll', listenForScroll);
+    }, [getUserId, postIds]);
+
+    useEffect(() => {
+        loadingMore.current = false;
+    }, [postIds.length]);
 
     return(
         <Flex className={styles['profile-overview']}>
