f7100684ef7f69ffba36ef2de031ccaab4b1df3b
Poxie <poxen12@hotmail.com>
2022-01-13 04:12:29 +0100

    Navigate to post page on comment click

--- frontend/components/user/CommentButton.tsx
+++ frontend/components/user/CommentButton.tsx
@@ -4,11 +4,13 @@ import styles from '../../styles/User.module.scss';
 import { CommentsIcon } from '../../icons/CommentsIcon';
 import { useAppSelector } from '../../redux/hooks';
 import { selectPostById } from '../../redux/selectors';
+import { useRouter } from 'next/router';
 
 type Props = {
     postId: string;
 }
 export const CommentButton: React.FC<Props> = ({ postId }) => {
+    const router = useRouter();
     const post = useAppSelector(state => selectPostById(state, postId));
     const { commentCount } = post;
 
@@ -16,6 +18,7 @@ export const CommentButton: React.FC<Props> = ({ postId }) => {
         <Flex
             className={styles['footer-item']}
             alignItems={'center'}
+            onClick={() => router.push(`/posts/${postId}`)}
         >
             <CommentsIcon />
             <span className={styles['footer-item-text']}>
