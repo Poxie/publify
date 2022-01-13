1201224e1b262815a93a74cb200b1edcdbc06aa2
Poxie <poxen12@hotmail.com>
2022-01-13 02:22:05 +0100

    Added user avatar to add comment component

--- frontend/components/posts/AddComment.tsx
+++ frontend/components/posts/AddComment.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import { CommentInput } from './CommentInput';
 import styles from '../../styles/Post.module.scss';
+import { Flex } from '../Flex';
+import { useAuth } from '../../contexts/AuthProvider';
+import { Avatar } from '../Avatar';
 
 type Props = {
     postId: string;
 }
 export const AddComment: React.FC<Props> = ({ postId }) => {
+    const { user } = useAuth();
+    const { avatar, displayName } = user;
+
     return(
-        <div className={styles['add-comment']}>
+        <Flex className={styles['add-comment']} alignItems={'center'}>
+            <Avatar 
+                avatar={avatar}
+                name={displayName}
+                className={styles['add-comment-avatar']}
+            />
             <CommentInput 
                 parentId={postId}
                 type={'post'}
             />
-        </div>
+        </Flex>
     )
 }
