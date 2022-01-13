a247032566df1e56562fac5c8f38f68ebe2129b3
Poxie <poxen12@hotmail.com>
2022-01-13 03:21:26 +0100

    Made it display media item based on media param

--- frontend/components/posts/PostMedia.tsx
+++ frontend/components/posts/PostMedia.tsx
@@ -1,14 +1,26 @@
-import react, { useState } from 'react';
+import react, { useEffect, useState } from 'react';
 import { MediaItem } from './MediaItem';
 import styles from '../../styles/Post.module.scss';
 import { useAppSelector } from '../../redux/hooks';
 import { selectActivePost } from '../../redux/selectors';
 import { MediaHeader } from './MediaHeader';
 import { Flex } from '../Flex';
+import { useRouter } from 'next/router';
 
 export const PostMedia = () => {
-    const { media } = useAppSelector(state => selectActivePost(state));
+    const router = useRouter();
+    const { media, id } = useAppSelector(state => selectActivePost(state));
     const [activeIndex, setActiveIndex] = useState(0);
+
+    // Updating active index by query params
+    useEffect(() => {
+        const mediaIndex = Array.isArray(router.query.media) ? parseInt(router.query.media[0]) : parseInt(router.query.media);
+        if(isNaN(mediaIndex) || mediaIndex > media.length) {
+            router.replace(`/posts/${id}?media=0`);
+            return
+        }
+        setActiveIndex(mediaIndex);
+    }, [router.query]);
     
     return(
         <div className={styles['media']}>
