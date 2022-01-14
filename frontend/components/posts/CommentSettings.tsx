4c0d28280e52d6c34fcc6281b8758149c83b2ae2
Poxie <poxen12@hotmail.com>
2022-01-14 02:11:19 +0100

    Added author block for deleting comments

--- frontend/components/posts/CommentSettings.tsx
+++ frontend/components/posts/CommentSettings.tsx
@@ -4,24 +4,36 @@ import { OptionsIcon } from '../../icons/OptionsIcon';
 import { Options } from '../Options';
 import { useDispatch } from 'react-redux';
 import { removeComment } from '../../redux/actions';
+import { useAuth } from '../../contexts/AuthProvider';
+import { useAppSelector } from '../../redux/hooks';
+import { selectCommentAuthor, selectReplyAuthor } from '../../redux/selectors';
 
 type Props = {
     commentId: string;
     replyId?: string;
 }
 export const CommentSettings: React.FC<Props> = ({ commentId, replyId }) => {
+    const { user } = useAuth();
     const [settingsVisible, setSetingsVisible] = useState(false);
     const dispatch = useDispatch();
+    const author = useAppSelector(state => selectCommentAuthor(state, commentId)) || useAppSelector(state => selectReplyAuthor(state, commentId, replyId));
 
+    // Deleting comment
     const deleteComment = () => {
         dispatch(removeComment(commentId, replyId));
+    }
+
+    // Creating settings items
+    const items = [{type: 'danger', text: 'Report comment', onClick: () => {}}]
+    if(author.id === user?.id) {
+        items.push({type: 'danger', text: 'Delete comment', onClick: deleteComment})
     }
 
     const className = [styles['comment-settings'], settingsVisible ? styles['visible'] : ''].join(' ');
     return(
         <div className={className}>
             <Options
-                items={[{type: 'danger', text: 'Delete comment', onClick: deleteComment}]}
+                items={items}
                 onChange={setSetingsVisible}
             >
                 <OptionsIcon />
