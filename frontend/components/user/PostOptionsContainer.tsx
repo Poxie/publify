e3b0f6cffba0555c837969e6583bb1bed1f42e5a
Poxie <poxen12@hotmail.com>
2022-01-16 16:11:58 +0100

    Moved profile posts options into options component instead

--- frontend/components/user/PostOptionsContainer.tsx
+++ frontend/components/user/PostOptionsContainer.tsx
@@ -1,34 +1,47 @@
-import React, { useState } from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useAuth } from '../../contexts/AuthProvider';
 import { OptionsIcon } from '../../icons/OptionsIcon';
+import { removePost } from '../../redux/actions';
+import { useAppSelector } from '../../redux/hooks';
 import styles from '../../styles/User.module.scss';
+import { Options } from '../Options';
 import { PostOptions } from './PostOptions';
 
+type Item = {
+    text: string;
+    type: 'danger' | 'default';
+    onClick: () => void;
+}
 type Props = {
     authorId: string;
     postId: string;
 }
 export const PostOptionsContainer: React.FC<Props> = ({ authorId, postId }) => {
-    const [options, setOptions] = useState(false);
+    const dispatch = useDispatch();
+    const { user } = useAuth();
+    const [items, setItems] = useState<Item[]>([{text: 'Report post', type: 'danger', onClick: () => {}}]);
 
-    const toggle = () => {
-        setOptions(previous => !previous);
+    useEffect(() => {
+        // If user is author, show delete option
+        if(user?.id === authorId && !items.map(item => item.text).includes('Delete post')) {
+            items.unshift({text: 'Delete post', type: 'danger', onClick: deletePost});
+        }
+    }, [user]);
+
+    // Deleting post
+    const deletePost = () => {
+        dispatch(removePost(postId));
     }
 
     return(
-        <div className={styles['post-options']}>
-            <div 
-                className={options ? styles['active'] : ''}
-                onClick={toggle}
+        <div style={{position: 'relative', zIndex: 15}}>
+            <Options 
+                items={items}
             >
                 <OptionsIcon />
-            </div>
-            {options && (
-                <PostOptions
-                    postId={postId}
-                    authorId={authorId}
-                    close={toggle}
-                />
-            )}
+            </Options>
         </div>
     )
 }
