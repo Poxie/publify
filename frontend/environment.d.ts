61adb8971b4661e0678b40ba5dd1ba4cce493fe6
Poxie <poxen12@hotmail.com>
2022-01-22 03:36:15 +0100

    Removed duplicate

---
+++ frontend/environment.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            NEXT_PUBLIC_API_ENDPOINT: string;
+            NEXT_PUBLIC_WEBSITE_NAME: string;
+            NEXT_PUBLIC_IMAGE_ENDPOINT: string;
+            NEXT_PUBLIC_IMAGE_HOST: string;
+            NEXT_PUBLIC_WEBSITE_ORIGIN: string;
+        }
+    }
+}
+
+export {};
