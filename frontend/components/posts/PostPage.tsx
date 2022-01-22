e1a9dcb89d9963b9f6a1408d0368b9b6451c5da0
Poxie <poxen12@hotmail.com>
2022-01-22 03:53:47 +0100

    Preventing setting state again if already exists

--- frontend/components/posts/PostPage.tsx
+++ frontend/components/posts/PostPage.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { setPost } from '../../redux/actions';
+import { useAppSelector } from '../../redux/hooks';
+import { selectActivePostId } from '../../redux/selectors';
 import { PostType } from '../../utils/types';
 import { Post } from './Post';
 
 export const PostPage: React.FC<PostType> = React.memo((post) => {
     const dispatch = useDispatch();
+    const activeId = useAppSelector(state => selectActivePostId(state));
 
     // Store data in redux store
-    dispatch(setPost(post));
+    if(activeId !== post.id) {
+        dispatch(setPost(post));
+    }
 
     return(
         <Post />
