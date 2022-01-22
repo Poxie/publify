5ac06354d3a6dbbb339ff1adc15b2397b2d3349a
Poxie <poxen12@hotmail.com>
2022-01-22 06:58:31 +0100

    Limited author name width

--- frontend/components/posts/CommentAuthor.tsx
+++ frontend/components/posts/CommentAuthor.tsx
@@ -4,22 +4,25 @@ import { HasPopout } from '../../popouts/HasPopout';
 import { UserPopout } from '../../popouts/user-popout/UserPopout';
 import styles from '../../styles/Post.module.scss';
 import { UserType } from '../../utils/types';
+import { Flex } from '../Flex';
 
 export const CommentAuthor: React.FC<UserType> = (user) => {
     const { username, displayName } = user;
     return(
-        <HasPopout
-            popout={(
-                <UserPopout 
-                    {...user}
-                />
-            )}
-        >
-            <div className={styles['comment-author']}>
-                <Link href={`/${username}`}>
-                    {displayName}
-                </Link>
-            </div>
-        </HasPopout>
+        <Flex>
+            <HasPopout
+                popout={(
+                    <UserPopout 
+                        {...user}
+                    />
+                )}
+            >
+                <div className={styles['comment-author']}>
+                    <Link href={`/${username}`}>
+                        {displayName}
+                    </Link>
+                </div>
+            </HasPopout>
+        </Flex>
     )
 }
