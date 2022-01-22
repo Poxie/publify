19acc1bfa1bd8fa2a7bcedcd70785e56c23d10d4
Poxie <poxen12@hotmail.com>
2022-01-22 07:09:43 +0100

    Click on avatar to get to profile

--- frontend/popouts/user-popout/UserPopout.tsx
+++ frontend/popouts/user-popout/UserPopout.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import styles from '../../styles/UserPopout.module.scss';
 import { UserProfile } from '../../components/UserProfile';
 import { UserType } from '../../utils/types';
+import { useRouter } from 'next/router';
 
 export const UserPopout: React.FC<UserType> = (user) => {
+    const router = useRouter();
+
     return(
         <UserProfile 
             {...user}
             bannerStyle={styles['banner']}
             avatarStyle={styles['avatar']}
+            avatarHoverText={'View Profile'}
+            onAvatarClick={() => router.push(`/${user.username}`)}
         />
     )
 }
