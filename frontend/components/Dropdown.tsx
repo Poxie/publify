5cc01dcaedcaf0e75588911189e7bc4b6564cd98
Poxie <poxen12@hotmail.com>
2022-01-23 14:06:02 +0100

    Added transparent type

--- frontend/components/Dropdown.tsx
+++ frontend/components/Dropdown.tsx
@@ -5,16 +5,19 @@ import { DropdownArrowIcon } from '../icons/DropdownArrowIcon';
 import styles from '../styles/Dropdown.module.scss';
 import { Flex } from './Flex';
 
-type Item = {
+export type DropdownItem = {
     onClick?: () => void;
     text: string;
+    id: string;
 }
 type Props = {
-    items: Item[];
-    activeItemText: string;
+    items: DropdownItem[];
+    activeItemId?: string;
+    className?: string;
+    type?: 'primary' | 'transparent';
 }
 
-const DropdownItem: React.FC<Item> = ({ text, onClick }) => {
+const DropdownItem: React.FC<DropdownItem> = ({ text, onClick }) => {
     return(
         <div 
             className={styles['item']}
@@ -24,10 +27,10 @@ const DropdownItem: React.FC<Item> = ({ text, onClick }) => {
         </div>
     )
 }
-export const Dropdown: React.FC<Props> = ({ items, activeItemText }) => {
+export const Dropdown: React.FC<Props> = ({ items, activeItemId, className, type }) => {
     const [visible, setVisible] = useState(false);
     const [width, setWidth] = useState(0);
-    const activeItem = items.find(item => item.text === activeItemText);
+    const activeItem = items.find(item => item.id === activeItemId) || items[0];
     const ref = useRef<HTMLDivElement>(null);
 
     // Updating fixed element's width on mount and resize
@@ -66,8 +69,10 @@ export const Dropdown: React.FC<Props> = ({ items, activeItemText }) => {
     }
 
     const containerClassName = [
-        styles['container'], 
-        visible ? styles['open'] : ''
+        styles['container'],
+        visible ? styles['open'] : '',
+        className && className,
+        type === 'transparent' && styles['transparent']
     ].join(' ');
     return(
         <div className={containerClassName} ref={ref}>
@@ -77,7 +82,7 @@ export const Dropdown: React.FC<Props> = ({ items, activeItemText }) => {
                 className={styles['active-item']} 
                 onClick={toggleVisible}
             >
-                <DropdownItem text={activeItem.text} />
+                <DropdownItem id={activeItem.id} text={activeItem.text} />
                 <DropdownArrowIcon />
             </Flex>
             {visible && (
