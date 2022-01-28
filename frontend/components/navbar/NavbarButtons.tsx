f391f5a7636523ca9c51c7364529ae1e6e7ed9ee
Poxie <poxen12@hotmail.com>
2022-01-28 09:40:25 +0100

    Hide navbar buttons if not logged in

--- frontend/components/navbar/NavbarButtons.tsx
+++ frontend/components/navbar/NavbarButtons.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
-import styles from '../../styles/Navbar.module.scss';
 import { SettingsIcon } from '../../icons/SettingsIcon';
 import { Flex } from '../Flex';
 import { Notifications } from './notifications/Notificaitons';
 import { NavbarButton } from './NavbarButton';
+import { useAuth } from '../../contexts/AuthProvider';
 
 export const NavbarButtons = () => {
+    const { user } = useAuth();
+
     return(
-        <Flex>
-            <Notifications />
-            <NavbarButton path={'settings'}>
-                <SettingsIcon />
-            </NavbarButton>
-        </Flex>
+        <>
+        {user && (
+            <Flex>
+                <Notifications />
+                <NavbarButton path={'settings'}>
+                    <SettingsIcon />
+                </NavbarButton>
+            </Flex>
+        )}
+        </>
     )
 }
