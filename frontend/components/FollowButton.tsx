2eb081cc745ca3634a1276c9f4e009b0ea7e11de
Poxie <poxen12@hotmail.com>
2022-01-28 06:55:35 +0100

    Added auth check on follow

--- frontend/components/FollowButton.tsx
+++ frontend/components/FollowButton.tsx
@@ -1,8 +1,11 @@
 import { useTranslation } from 'next-i18next';
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { useModal } from '../contexts/ModalProvider';
+import { LoginModal } from '../modals/login/LoginModal';
 import { createFollowAction, destroyFollowAction } from '../redux/actions';
 import { createFollow, destroyFollow } from '../utils';
+import { isAuthError } from '../utils/errors';
 import { Button } from './Button';
 
 interface Props {
@@ -11,6 +14,7 @@ interface Props {
 }
 export const FollowButton: React.FC<Props> = ({ userId, isFollowing }) => {
     const { t } = useTranslation();
+    const { setModal } = useModal();
     const dispatch = useDispatch();
 
     const onClick = async () => {
@@ -18,7 +22,13 @@ export const FollowButton: React.FC<Props> = ({ userId, isFollowing }) => {
             await destroyFollow(userId);
             dispatch(destroyFollowAction());
         } else {
-            await createFollow(userId);
+            const response = await createFollow(userId).catch(error => {
+                if(isAuthError(error)) {
+                    setModal(<LoginModal />)
+                }
+            });
+            if(!response) return;
+
             dispatch(createFollowAction());
         }
     }
