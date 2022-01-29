574ef6749285e3fab223a495241ab7c113b5c220
Poxie <poxen12@hotmail.com>
2022-01-29 15:12:57 +0100

    Added flex container

--- frontend/components/navbar/NavbarInput.tsx
+++ frontend/components/navbar/NavbarInput.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import styles from '../../styles/Navbar.module.scss';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { Input } from '../Input';
 import { useTranslation } from 'next-i18next';
+import { Flex } from '../Flex';
 
 export const NavbarInput = () => {
     const { t } = useTranslation();
@@ -14,11 +16,14 @@ export const NavbarInput = () => {
     }
 
     return(
-        <Input 
-            placeholder={t('navbarSearch')}
-            type={'secondary'}
-            onChange={setSearch}
-            onSubmit={onSubmit}
-        />
+        <Flex>
+            <Input 
+                placeholder={t('navbarSearch')}
+                type={'secondary'}
+                onChange={setSearch}
+                onSubmit={onSubmit}
+                className={styles['input']}
+            />
+        </Flex>
     )
 }
