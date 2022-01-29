38749502829fd4e1c4a7b7512d8a8b64cbb113b9
Poxie <poxen12@hotmail.com>
2022-01-29 20:23:39 +0100

    Added loading profile stats

--- frontend/components/loading/LoadingProfile.tsx
+++ frontend/components/loading/LoadingProfile.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from '../../styles/Loading.module.scss';
+import { Flex } from '../Flex';
 import { LoadingAvatar } from "./LoadingAvatar"
 import { LoadingBanner } from './LoadingBanner';
 import { LoadingText } from "./LoadingText"
@@ -11,6 +12,10 @@ export const LoadingProfile = () => {
             <LoadingAvatar size={70} />
             <LoadingText minWidth={80} maxWidth={170} height={24} />
             <LoadingText minWidth={120} maxWidth={190} height={16} />
+            <Flex style={{marginTop: 0}}>
+                <LoadingText height={16} minWidth={50} maxWidth={50} style={{marginRight: 'calc(var(--spacing) / 2)'}} />
+                <LoadingText height={16} minWidth={50} maxWidth={50} />
+            </Flex>
         </div>
     )
 }
