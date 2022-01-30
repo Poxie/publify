11075a7d216c0e190c940d9b76c1efd9cea507f9
Poxie <poxen12@hotmail.com>
2022-01-30 18:19:51 +0100

    Removed unnecessary imports

--- frontend/components/user/ProfileOverview.tsx
+++ frontend/components/user/ProfileOverview.tsx
@@ -1,17 +1,8 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
 import styles from '../../styles/User.module.scss';
-import { useRouter } from 'next/router';
-import { Flex } from '../Flex';
-import { ProfilePosts } from './ProfilePosts';
-import { ProfileMediaPreview } from './ProfileMediaPreview';
-import { useDispatch, useSelector } from 'react-redux';
-import { fetchUserPosts, loadMorePosts } from '../../redux/actions';
-import { useAppSelector } from '../../redux/hooks';
-import { selectPostIds } from '../../redux/selectors';
-import { CreatePost } from './CreatePost';
-import { LoadingPosts } from '../loading/LoadingPosts';
 import { ProfileSidebar } from './ProfileSidebar';
 import { ProfileMain } from './ProfileMain';
+import { Flex } from '../Flex';
 
 export const ProfileOverview = () => {
     return(
