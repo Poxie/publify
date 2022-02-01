f45ba85578255e745bb3f39875749678025e1954
Poxie <poxen12@hotmail.com>
2022-02-01 19:33:53 +0100

    Handling different 'user states'

--- frontend/layouts/SettingsLayout.tsx
+++ frontend/layouts/SettingsLayout.tsx
@@ -2,11 +2,25 @@ import React from 'react';
 import styles from '../styles/Settings.module.scss';
 import { Flex } from '../components/Flex';
 import { SettingsSidebar } from '../components/settings/SettingsSidebar';
+import { useAuth } from '../contexts/AuthProvider';
+import { useRouter } from 'next/router';
 
 type Props = {
     children: any;
 }
 export const SettingsLayout: React.FC<Props> = ({ children }) => {
+    const router = useRouter();
+    const { user } = useAuth();
+    
+    // If user is loading
+    if(user === null) return null;
+
+    // If user is not logged in
+    if(user === undefined) {
+        router.replace('/login');
+        return null;
+    }
+
     return(
         <Flex className={styles['layout']}>
             <SettingsSidebar />
