50f508fab02fb257586b576ee2d2c2553d181ded
Poxie <poxen12@hotmail.com>
2022-02-01 18:47:29 +0100

    Moved profile posts into using PartialPost component

--- frontend/components/user/ProfilePost.tsx
+++ frontend/components/user/ProfilePost.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { PostType } from '../../utils/types';
-import styles from '../../styles/User.module.scss';
-import { ProfilePostHeader } from './ProfilePostHeader';
-import { ProfilePostMedia } from './ProfilePostMedia';
-import { ProfilePostFooter } from './ProfilePostFooter';
 import { useAppSelector } from '../../redux/hooks';
 import { selectPostById } from '../../redux/selectors';
+import { PartialPost } from '../PartialPost';
 
 type Props = {
     postId: string;
@@ -14,27 +10,7 @@ export const ProfilePost: React.FC<Props> = ({ postId }) => {
     // Getting post
     const post = useAppSelector(state => selectPostById(state, postId));
 
-    const { author, content, media, likes, likeCount, createdAt } = post;
-
     return(
-        <div className={styles['post']}>
-            <ProfilePostHeader 
-                postId={postId}
-                authorId={author.id}
-                createdAt={createdAt}
-            />
-
-            {content}
-
-            {media.length !== 0 && (
-                <ProfilePostMedia 
-                    media={media}
-                />
-            )}
-
-            <ProfilePostFooter 
-                postId={postId}
-            />
-        </div>
+        <PartialPost {...post} />
     )
 }
