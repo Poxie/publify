e1289532c9989093067ef75984de01db07e51e55
Poxie <poxen12@hotmail.com>
2022-02-01 18:47:11 +0100

    Making sure footer is at complete bottom of container

--- frontend/components/PartialPost.tsx
+++ frontend/components/PartialPost.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '../styles/PartialPost.module.scss';
 import { PostType } from '../utils/types';
+import { Flex } from './Flex';
 import { PartialPostContent } from './PartialPostContent';
 import { PartialPostFooter } from './PartialPostFooter';
 import { PartialPostHeader } from './PartialPostHeader';
@@ -9,20 +10,22 @@ import { PartialPostMedia } from './PartialPostMedia';
 export const PartialPost: React.FC<PostType & {displayMedia?: boolean, className?: string}> = ({ className: _className, displayMedia=true, id, isLiked, likeCount, commentCount, author, createdAt, content, media }) => {
     const className = [styles['container'], _className].join(' ');
     return(
-        <div className={className}>
-            <PartialPostHeader 
-                author={author}
-                createdAt={createdAt}
-            />
-            <PartialPostContent content={content} />
-            {media?.length !== 0 && displayMedia && (
-                <PartialPostMedia media={media} postId={id} />
-            )}
-            {media?.length !== 0 && !displayMedia && (
-                <span className={styles['has-media-hidden']}>
-                    Has media attached
-                </span>
-            )}
+        <Flex className={className} justifyContent={'space-between'} flexDirection={'column'}>
+            <div>
+                <PartialPostHeader 
+                    author={author}
+                    createdAt={createdAt}
+                />
+                <PartialPostContent content={content} />
+                {media?.length !== 0 && displayMedia && (
+                    <PartialPostMedia media={media} postId={id} />
+                )}
+                {media?.length !== 0 && !displayMedia && (
+                    <span className={styles['has-media-hidden']}>
+                        Has media attached
+                    </span>
+                )}
+            </div>
             <PartialPostFooter
                 id={id}
                 isLiked={isLiked}
@@ -30,6 +33,6 @@ export const PartialPost: React.FC<PostType & {displayMedia?: boolean, className
                 commentCount={commentCount}
                 mediaCount={media.length}
             />
-        </div>
+        </Flex>
     )
 }
