c898fa01cd495d113503371ef78c431a1da7fba5
Poxie <poxen12@hotmail.com>
2022-02-02 19:38:32 +0100

    Added login button if user is not logged in

--- frontend/components/navbar/NavbarButtons.tsx
+++ frontend/components/navbar/NavbarButtons.tsx
@@ -5,21 +5,38 @@ import { Notifications } from './notifications/Notificaitons';
 import { NavbarButton } from './NavbarButton';
 import { useAuth } from '../../contexts/AuthProvider';
 import { NavbarUser } from './NavbarUser';
+import { Button } from '../Button';
+import { useModal } from '../../contexts/ModalProvider';
+import { LoginModal } from '../../modals/login/LoginModal';
+import { useTranslation } from 'next-i18next';
 
 export const NavbarButtons = () => {
     const { user } = useAuth();
 
     return(
-        <>
-        {user && (
-            <Flex justifyContent={'flex-end'} alignItems={'center'}>
-                <Notifications />
-                <NavbarButton path={'settings/profile'}>
-                    <SettingsIcon />
-                </NavbarButton>
-                <NavbarUser />
-            </Flex>
-        )}
-        </>
+        <Flex justifyContent={'flex-end'} alignItems={'center'}>
+            {user ? (
+                <>
+                    <Notifications />
+                    <NavbarButton path={'settings/profile'}>
+                        <SettingsIcon />
+                    </NavbarButton>
+                    <NavbarUser />
+                </>
+            ) : (
+                <LoginButton />
+            )}
+        </Flex>
+    )
+}
+
+const LoginButton: React.FC = () => {
+    const { t } = useTranslation();
+    const { setModal } = useModal();
+
+    return(
+        <Button onClick={() => setModal(<LoginModal />)}>
+            {t('login')}
+        </Button>
     )
 }
